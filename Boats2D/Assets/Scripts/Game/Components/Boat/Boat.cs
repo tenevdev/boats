5b680c8d3bef5c0b6fdf8bef4bde30f8892c298c
Tencho Tenev <tenev.dev@outlook.com>
2015-06-10 20:16:46 +0300

    Update hit points bar

---
+++ Boats2D/Assets/Scripts/Game/Components/Boat/Boat.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts.Game.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Game.Components
+{
+    public class Boat : Creatable
+    {
+        private int passengerCount = 0;
+
+        public HitPointsBar hitPointsBar;
+
+        public void Start()
+        {
+            base.Start();
+            if (this.hitPointsBar == null)
+            {
+                this.hitPointsBar = this.GetComponentInChildren<HitPointsBar>();
+            }
+
+            this.HitReceived += this.hitPointsBar.UpdateBar;
+        }
+    }
+}
---
+++ Boats2D/Assets/Scripts/Game/Components/Boat/HitPointsBar.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Scripts.Game.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Game.Components
+{
+    public class HitPointsBar : MonoBehaviour
+    {
+        private SpriteRenderer barRenderer;
+
+        public void Start()
+        {
+            this.barRenderer = this.GetComponent<SpriteRenderer>();
+        }
+
+        public void UpdateBar(Hittable sender, HitEventArgs args)
+        {
+            this.barRenderer.color = Color.Lerp(Color.red, Color.green, args.HitPointsPercent);
+            this.transform.localScale = new Vector3(args.HitPointsPercent, 1, 1);
+        }
+    }
+}
