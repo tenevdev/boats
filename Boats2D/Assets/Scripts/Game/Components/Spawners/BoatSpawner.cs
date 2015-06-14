6d0d2f4f06a2a535fdc69f04d657a1a535b4611e
Tencho Tenev <tenev.dev@outlook.com>
2015-06-14 16:10:45 +0300

    Add building sprites

---
+++ Boats2D/Assets/Scripts/Game/Components/Spawners/BoatSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Game.Components.Spawners
+{
+    public class BoatSpawner : Spawner
+    {
+        public override void Start()
+        {
+            // Do not start creating boats
+        }
+
+        // TODO: Show/hide UI for creating boats on click
+    }
+}
