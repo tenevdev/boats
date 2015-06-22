064e36a2f4ab9e500ba8de20473ce7bb9ff63279
Tencho Tenev <tenev.dev@outlook.com>
2015-06-22 17:56:49 +0300

    Add theme song

---
+++ Boats2D/Assets/Scripts/Game/Components/Persistor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Game.Components
+{
+    public class Persistor : MonoBehaviour
+    {
+        public void Awake()
+        {
+            // There should be only one persistor
+            if (GameObject.FindGameObjectsWithTag(this.tag).Count() != 1)
+            {
+                Destroy(this.transform.gameObject);
+            }
+            else
+            {
+                // Keep object alive on all scenes
+                DontDestroyOnLoad(this.gameObject);
+            }
+        }
+    }
+}
