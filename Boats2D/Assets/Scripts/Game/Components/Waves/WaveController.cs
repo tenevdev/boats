4f91810ccea9ab7eb15401d739f5a3fe72da1ba0
Tencho Tenev <tenev.dev@outlook.com>
2015-06-22 15:46:53 +0300

    Optimize input

--- Boats2D/Assets/Scripts/Game/Components/Waves/WaveController.cs
+++ Boats2D/Assets/Scripts/Game/Components/Waves/WaveController.cs
@@ -7,19 +7,37 @@ namespace Assets.Scripts.Game.Components
     /// <summary>
     /// Create waves based on user input
     /// </summary>
-    public class WaveController : MonoBehaviour, IPointerClickHandler
+    public class WaveController : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler, IPointerClickHandler
     {
         public Transform ripple;
-
-        //public void OnMouseUp()
-        //{
-        //    Debug.Log(Input.mousePosition);
-        //    Instantiate(this.ripple, Input.mousePosition, Quaternion.identity);
-        //}
+        public int dragInterval = 10;
+        private int count = 0;
 
         public void OnPointerClick(PointerEventData eventData)
         {
-            Debug.Log(Input.mousePosition);
+            Instantiate(this.ripple, eventData.position, Quaternion.identity);
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            if (this.count == this.dragInterval)
+            {
+                Instantiate(this.ripple, eventData.position, Quaternion.identity);
+                count = 0;
+            }
+            else
+            {
+                count++;
+            }
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            Instantiate(this.ripple, eventData.position, Quaternion.identity);
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
             Instantiate(this.ripple, eventData.position, Quaternion.identity);
         }
     }
